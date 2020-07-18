43b6162f9b446bb26619baec48d9b7ac9e1e099d
Roykssop <herna14@gmail.com>
2020-07-18 13:23:36 -0300

    Build to deploy on Github pages

---
+++ docs/precache-manifest.ad83544e6616e52a6ef29c992f701b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80854eb0bdbdbeb124894a5ef6956a34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "998f1aca6e4a6c3a115e",
+    "url": "/static/css/main.7e58bb79.chunk.css"
+  },
+  {
+    "revision": "468ea37b74389e030585",
+    "url": "/static/js/2.27bfd943.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27bfd943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998f1aca6e4a6c3a115e",
+    "url": "/static/js/main.4e29fda5.chunk.js"
+  },
+  {
+    "revision": "ca2b345afa23de96780b",
+    "url": "/static/js/runtime-main.eec11a41.js"
+  }
+]);
